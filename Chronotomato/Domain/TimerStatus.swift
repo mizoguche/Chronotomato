0c3bf2f4fa720d56917e741921473e54ca3be560
Michiaki Mizoguchi <michiaki.mizoguchi@gmail.com>
2024-11-04 07:12:01 +0900

    Update TimerStatus
    Implement TimerStatus transition.
    
    ```mermaid
    stateDiagram-v2
        [*] --> Stopped
        Stopped --> Playing: play
        Playing --> Alarming: alarm
        Playing --> Stopped: stop
        Playing --> Playing: tick
        Alarming --> Stopped: stop
    ```

--- Chronotomato/Domain/TimerStatus.swift
+++ Chronotomato/Domain/TimerStatus.swift
@@ -1,40 +1,79 @@
 import Foundation
 
-enum TimerStatus {
-    case playing(presetTime: Int, remainingTime: Int)
-    case alarming
-    case stopped(presetTime: Int)
+enum TimerError: Error {
+    case illegalStatusTransition
+}
+
+let long = 25 * 60
+let short = 5 * 60
+let presets: [Int] = [long, short]
+
+func formatTime(_ time: Int) -> String {
+    let formatter = DateComponentsFormatter()
+    formatter.unitsStyle = .positional
+    formatter.zeroFormattingBehavior = .pad
+    formatter.allowedUnits = [.minute, .second]
+    return formatter.string(from: TimeInterval(time)) ?? "Error"
+}
+
+enum TimerStatus: Equatable {
+    case playing(currentPresetIndex: Int, remainingTime: Int)
+    case alarming(currentPresetIndex: Int)
+    case stopped(currentPresetIndex: Int)
 
     var remainingTime: String {
         switch self {
         case .playing(_, let remainingTime):
-            let formatter = DateComponentsFormatter()
-            formatter.unitsStyle = .positional
-            formatter.zeroFormattingBehavior = .pad
-            formatter.allowedUnits = [.minute, .second]
-
-            let interval = TimeInterval(remainingTime)
-            return formatter.string(from: interval) ?? "Error"
-        case .alarming:
-            return "00:00"
-        case .stopped:
-            return "00:00"
+            return formatTime(remainingTime)
+        case .alarming, .stopped:
+            return formatTime(0)
         }
     }
 
-    func toNextStatus() -> TimerStatus {
+    func play() throws(TimerError) -> TimerStatus {
         switch self {
-        case .playing(let presetTime, let remainingTime):
+        case .playing:
+            return self
+        case .alarming:
+            throw TimerError.illegalStatusTransition
+        case .stopped(let currentPresetIndex):
+            let nextInterval = presets[currentPresetIndex]
+            return .playing(currentPresetIndex: currentPresetIndex, remainingTime: nextInterval)
+        }
+    }
+
+    func stop() throws(TimerError) -> TimerStatus {
+        switch self {
+        case .playing(let currentPresetIndex, _):
+            return .stopped(currentPresetIndex: currentPresetIndex)
+        case .alarming(let currentPresetIndex):
+            let nextPresetIndex = (currentPresetIndex + 1) % presets.count
+            return .stopped(currentPresetIndex: nextPresetIndex)
+        case .stopped:
+            throw TimerError.illegalStatusTransition
+        }
+    }
+
+    func alarm() throws(TimerError) -> TimerStatus {
+        switch self {
+        case .playing(let currentPresetIndex, _):
+            return .alarming(currentPresetIndex: currentPresetIndex)
+        case .alarming, .stopped:
+            throw TimerError.illegalStatusTransition
+        }
+    }
+
+    func tick() throws(TimerError) -> TimerStatus {
+        switch self {
+        case .playing(let currentPresetIndex, let remainingTime):
             let remainingTime = remainingTime - 1
             if remainingTime > 0 {
-                return .playing(presetTime: presetTime, remainingTime: remainingTime)
+                return .playing(currentPresetIndex: currentPresetIndex, remainingTime: remainingTime)
             } else {
-                return .alarming
+                return .alarming(currentPresetIndex: currentPresetIndex)
             }
-        case .alarming:
-            return .stopped(presetTime: 0)
-        case .stopped(let presetTime):
-            return .playing(presetTime: presetTime, remainingTime: presetTime)
+        case .alarming, .stopped:
+            throw TimerError.illegalStatusTransition
         }
     }
 }
